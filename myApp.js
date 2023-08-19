443a64abaeb345b0c4994019e385a60b63906dd6
EjikeOdoh <ejikeodoh2020@gmail.com>
2023-08-19 21:42:55 +0100

    fourth task complete

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
 
+const staticPath = __dirname + "/public";
+
+app.use("/public", express.static(staticPath));
+
 app.get("/", (req, res) => {
   const path = __dirname + "/views/index.html";
   return res.sendFile(path);
